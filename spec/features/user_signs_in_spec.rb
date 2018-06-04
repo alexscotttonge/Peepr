19e5924085d1c95794d391d392cbfefe2e3dd0b7
Alex <alex@scotttonge.com>
2018-06-04 14:05:56 +0100

    Updated sign_in spec
    Added username field to sign_in spec.

--- spec/features/user_signs_in_spec.rb
+++ spec/features/user_signs_in_spec.rb
@@ -7,6 +7,7 @@ RSpec.feature "User signs in" do
     visit sign_in_path
     fill_in "session_email", with: user.email
     fill_in "session_password", with: user.password
+    fill_in "session_username", with: user.username
     click_button "Sign in"
 
     expect(page).to have_css "h1", text: "Peepr"
--- spec/features/user_visits_signup_page_spec.rb
+++ spec/features/user_visits_signup_page_spec.rb
@@ -9,10 +9,8 @@ RSpec.feature "User visits sign up page" do
     fill_in "user_email", with: "mrpink@dogs.com"
     fill_in "user_username", with: "Pinky"
     fill_in "user_password", with: "Dogs"
-
     click_button "Sign up"
-    save_and_open_page
-    
+
     expect(page).to have_css "h1", text: "Peepr"
   end
 end
