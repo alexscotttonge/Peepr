fa8f20f74d7b79d0145bf973ce927faacab83caa
Alex <alex@scotttonge.com>
2018-06-06 11:56:17 +0100

    User views own peep spec

---
+++ spec/features/user_views_own_peep_spec.rb
@@ -0,0 +1,16 @@
+require "rails_helper"
+include SignIn
+
+RSpec.feature "User views own peep" do
+  scenario "successfully" do
+    visit sign_in_path
+    sign_in_with_email
+
+    fill_in "peep_body", with: "Here me now"
+    click_on "Peep!"
+
+    click_on "whiterussian"
+
+    expect(page).to have_css "h1", text: "whiterussian"
+  end
+end
