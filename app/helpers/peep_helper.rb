f7bf74dff877119130ec27ba06577136b686689d
Alex <alex@scotttonge.com>
2018-06-12 11:45:05 +0100

    Refactor avatar method into new module

--- app/helpers/peep_helper.rb
+++ app/helpers/peep_helper.rb
@@ -1,12 +1,5 @@
 module PeepHelper
 
-  def avatar(user)
-    email_digest = Digest::MD5.hexdigest(user.email)
-    gravatar_url = "//www.gravatar.com/avatar/#{email_digest}"
-
-    image_tag gravatar_url
-  end
-
   def like_button(peep)
     if current_user.liked?(peep)
       link_to "Unlike", unlike_peep_path(peep), method: :delete
